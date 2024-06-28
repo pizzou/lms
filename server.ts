7b6c42d4572f3979a9419ae1f98391b3f58e0b3d
pizzou <pmpumuropizzou@gmail.com>
2024-06-28 12:08:13 +0200

    fourth commit

--- server.ts
+++ server.ts
@@ -1,23 +1,28 @@
-import {v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import http from "http";
 import connectDB from "./utils/db";
 import { initSocketServer } from "./socketServer";
 import { app } from "./app";
-require("dotenv").config();
+import dotenv from "dotenv"; // Import dotenv directly for clarity
+dotenv.config();
+
 const server = http.createServer(app);
 
-
-// cloudinary config
+// Cloudinary configuration
 cloudinary.config({
- cloud_name: process.env.CLOUD_NAME,
- api_key: process.env.CLOUD_API_KEY,
- api_secret: process.env.CLOUD_SECRET_KEY,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_SECRET_KEY,
 });
 
+// Initialize socket server
 initSocketServer(server);
 
-// create server
-server.listen(process.env.PORT, () => {
-    console.log(`Server is connected with port ${process.env.PORT}`);
-    connectDB();
+// Connect to database
+connectDB();
+
+// Start the server
+const PORT = process.env.PORT || 8000; // Default to 8000 if PORT is not defined in .env
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
