23f57d932d52b80e5ae45f3706d15d32dab38274
pizzou <pmpumuropizzou@gmail.com>
2024-07-01 20:28:00 +0200

    9 commit

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -8,7 +8,7 @@ import { updateAccessToken } from "../controllers/user.controller";
 // authenticated user
 export const isAutheticated = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
-    const access_token = req.headers.access_token as string;
+    const access_token = req.headers.accessToken as string;
     if (!access_token) {
       return next(
         new ErrorHandler("Please login to access this resource", 400)
