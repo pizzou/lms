c650ad721192bcfa097f50bba4728094fd8c4baa
pizzou <pmpumuropizzou@gmail.com>
2024-08-11 20:32:20 +0200

    006

--- app.ts
+++ app.ts
@@ -22,9 +22,12 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: true, 
-    credentials: true, 
-  })
-);
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }));
+  
+  app.options('*', cors());
 
 // api requests limit
 const limiter = rateLimit({
