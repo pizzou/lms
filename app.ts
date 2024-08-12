d79648c791c3a591a88d17026b8cb652b5643849
pizzou <pmpumuropizzou@gmail.com>
2024-08-12 21:28:37 +0200

    0003

--- app.ts
+++ app.ts
@@ -30,7 +30,7 @@ app.use(cookieParser());
 
 // CORS - Cross-Origin Resource Sharing
 app.use(cors({
-  origin: '*', // Allow all origins (use specific domains in production)
+  origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, 
